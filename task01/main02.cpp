4471c6889dc67093d2f98f35205498c1865ec242
polin-sv <polinasvetlay@icloud.com>
2026-06-04 22:13:36 +0300

    soft task

--- task01/main02.cpp
+++ task01/main02.cpp
@@ -5,7 +5,35 @@
 
 int main() {
 
-	Bolid* bolid = new Bolid(5);
+	Bus* setra = new Bus(20000);
+
+	Van* ford = new Van(15000);
+	Van* ram = new Van(10000);
+
+	Bolid* mercedes = new Bolid(20000000);
+	Bolid* ferrari = new Bolid(18000000);
+	Bolid* alpin = new Bolid(15000000);
+
+	int size = 6;
+
+	Transport** transports = new Transport * [size]
+		{setra, ford, ram, mercedes, ferrari, alpin};
+
+	CarCenter* center = new CarCenter(); 
+
+	int total = center->calculateTotalPrice(transports, size);
+
+	cout << "total: " << total << endl;
+
+	delete setra;
+	delete ford;
+	delete ram;
+	delete mercedes;
+	delete ferrari;
+	delete alpin;
+
+	delete[] transports;
+
 
 	return 0;
 }
